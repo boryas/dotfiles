69ef8c50de2fc4c547a150adb415c3cdf0d228d5
Boris Burkov <boris@bur.io>
2023-03-24 15:55:19 -0700

    arch .config

---
+++ .config/emacs-handroll/init.el
@@ -0,0 +1,45 @@
+(setq inhibit-startup-message t)
+(global-display-line-numbers-mode 1)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+;; Gruvbox
+(unless (package-installed-p 'gruvbox-theme)
+  (package-install 'gruvbox-theme))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" default))
+ '(package-selected-packages '(gruvbox-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(load-theme 'gruvbox-dark-medium t)
+
+;; Enable evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+(require 'evil)
+(evil-mode 1)
+
+;; set leader key in all states
+(evil-set-leader nil (kbd "C-SPC"))
+
+;; set leader key in normal state
+(evil-set-leader 'normal (kbd "SPC"))
+
+;; set local leader
+(evil-set-leader 'normal "," t)
+
+;; fake spacemacs
+(evil-define-key 'normal 'global (kbd "<leader>" .) 'find-file)
